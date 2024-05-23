0183e746e7b9e17f73e9d2565478c6b634f7c112
june-seok-kim <purifier772@gmail.com>
2024-05-23 21:15:59 +0900

    new: PNG Trimmer for ImageMagick added

---
+++ powershell/imagemagick/scriptio-PngTrimmer.ps1
@@ -0,0 +1,22 @@
+﻿# TODO: Override or not
+# TODO: Check for ImageMagick executable
+
+Write-Output "ImageMagick Macro : PNG Trimmer v0.1"
+
+$path = Read-Host "Enter target path"
+$filepath = $path + "\*"
+$files = Get-ChildItem -Path $filepath -Include "*.png"
+
+Write-Output $files.Count
+
+if ($files.Count -gt 0)
+{
+    $trimfolder = $path + "\trimmed\"
+    New-Item -Path $trimfolder -ItemType Directory
+}
+
+foreach ($file in $files)
+{
+    $trimfile = $trimfolder + $file.Name
+    magick convert $file.FullName -fuzz 1% -trim +repage $trimfile
+}
